ac2d03418ed5c07b0c097f7537ff74d6d73008ce
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-05 10:39:01 +0000

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,8 @@
+function hideWhenClicked() {
+  const element = document.getElementById('hide_this');
+  element.addEventListener('click', function(e){this.style.display = 'none'});
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  hideWhenClicked();
+});
